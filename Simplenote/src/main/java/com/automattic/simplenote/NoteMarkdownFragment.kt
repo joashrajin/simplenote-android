deac4190c44fe656297e865be8aa1e9c89155f25
Joash Rajin <joash@automattic.com>
2026-08-14 20:54:02 +0200

    Stop loading Google Fonts in markdown previews (#17)
    * Stop loading Google Fonts in markdown previews
    
    - Remove the injected remote stylesheet from generated preview HTML
    - Cover the shared formatter with a JVM regression test
    
    * Harden Google Fonts regression coverage
    
    Assert the removed provider directly in addition to the generic remote stylesheet check.

--- Simplenote/src/main/java/com/automattic/simplenote/NoteMarkdownFragment.kt
+++ Simplenote/src/main/java/com/automattic/simplenote/NoteMarkdownFragment.kt
@@ -50,7 +50,6 @@ class NoteMarkdownFragment : Fragment(), Bucket.Listener<Note> {
         @JvmStatic
         fun getMarkdownFormattedContent(cssContent: String, sourceContent: String): String {
             val header = "<html><head>" +
-                    "<link href=\"https://fonts.googleapis.com/css?family=Noto+Serif\" rel=\"stylesheet\">" +
                     "<meta name=\"viewport\" content=\"width=device-width,minimum-scale=1,initial-scale=1\">\n" +
                     cssContent + "</head><body>"
 
